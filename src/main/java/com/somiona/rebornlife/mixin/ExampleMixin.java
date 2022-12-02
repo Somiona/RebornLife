ae3e841261c61f71582eaa0267e99d9e0b02e2a4
Somiona <somionat@gmail.com>
2022-12-01 20:16:59 -0500

    init

---
+++ src/main/java/com/somiona/rebornlife/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package com.somiona.rebornlife.mixin;
+
+import com.somiona.rebornlife.RebornLife;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		RebornLife.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
